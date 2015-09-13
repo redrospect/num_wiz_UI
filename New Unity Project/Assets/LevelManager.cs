be9701f65d9b51d62701ffb2e8704b3403a7c8cc
redrospect <twikii@live.com>
2015-09-13 13:34:19 -0600

    9-13

---
+++ New Unity Project/Assets/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {  //needs to be public. the string allows for input in unity-tells what scene to load
+		Debug.Log ("Level load requested for:" +name);
+		Application.LoadLevel(name);
+
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Level quit requested");
+		Application.Quit ();
+	}
+
+	public void Back(string name) {
+		Debug.Log ("Back requested");
+		Application.LoadLevel(name);
+	}
+
+
+
+}
